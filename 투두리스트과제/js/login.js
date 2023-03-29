ed3619054d54e2b35bcfbcedd4f34cf0b56e7236
SH J <thdghk5429@gmail.com>
2023-03-29 10:13:59 +0900

    0329 todolist page

---
+++ 투두리스트과제/js/login.js
@@ -0,0 +1,22 @@
+const loginForm = document.querySelector("#login-form");
+
+loginForm.addEventListener("submit", onLogin)
+
+
+function onLogin(e){
+    
+    e.preventDefault();
+    
+    const loginId = document.querySelector("#login-id");
+    const greeting = document.querySelector("#greeting");
+    
+    greeting.innerHTML = `Welcome, ${loginId.value}!`;
+
+    greeting.classList.remove("hidden");
+    document.querySelector("#todo-form").classList.remove("hidden");
+    document.querySelector("#todolist").classList.remove("hidden");
+    document.querySelector("#tododone").classList.remove("hidden");
+    //document.querySelector("#listtitle").classList.remove("hidden");
+    loginForm.classList.add("hidden");
+
+}   
---
+++ 투두리스트과제/js/todolist.js
@@ -0,0 +1,95 @@
+const todoForm = document.querySelector("#todo-form");
+todoForm.addEventListener("submit", todoAdd);
+const done = document.querySelector("#tododone");
+// console.log(done);
+
+
+function todoAdd(e){
+    e.preventDefault();
+
+    const todoValue = todoForm.firstElementChild.value;
+    
+    const li = document.createElement("li");
+    const check = document.createElement("input");
+    check.type = "checkbox";
+    const text = document.createTextNode(todoValue);
+    const button = document.createElement("button");
+    button.innerHTML = "X";
+    const time = document.createElement("span");
+    
+    time.innerHTML = getClock();
+
+    li.appendChild(check);
+    li.appendChild(text);
+    li.appendChild(time); 
+    li.appendChild(button);
+
+    document.querySelector("#todolist").appendChild(li);
+
+    todoForm.firstElementChild.value = "";
+
+    /* 강사님 코드 */
+    // 할일이 추가되었다면 갯수를 세어서 출력 getAllcount();
+    // html에 count라는 id를 가진 h태그를 추가했음
+    //const countPrint = document.querySelector("#count");
+    //countPrint.innerHTML = `전체 할일 ${getAllcount()} | 완료한 할일 0`;
+
+    check.addEventListener("click", todoCheck);
+    button.addEventListener("click", todoDelete);
+
+    //const checkboxNumber = document.querySelectorAll("#todolist>li>input[type='checkbox']");
+    //const checkedBox = document.querySelectorAll("#todolist>li>input[type='checkbox']:checked");
+    //done.innerHTML = `전체 할일 ${checkboxNumber.length} | 완료한 할일 ${checkedBox.length}`;
+
+    count();
+}
+
+function count(){
+    const checkboxNumber = document.querySelectorAll("#todolist>li>input[type='checkbox']");
+    const checkedBox = document.querySelectorAll("#todolist>li>input[type='checkbox']:checked");
+    done.innerHTML = `전체 할일 ${checkboxNumber.length} | 완료한 할일 ${checkedBox.length}`;
+}
+
+function todoCheck(e){
+    // console.log(checkedBox.length);
+    const check = e.target;
+    const li = check.parentNode;
+    const checkboxNumber = document.querySelectorAll("#todolist>li>input[type='checkbox']");
+    const checkedBox = document.querySelectorAll("#todolist>li>input[type='checkbox']:checked");
+    if(check.checked){
+        li.style.color = "lightgray";
+        done.innerHTML = `전체 할일 ${checkboxNumber.length} | 완료한 할일 ${checkedBox.length}`;
+    } else {
+        check.parentNode.style.color = "";
+        done.innerHTML = `전체 할일 ${checkboxNumber.length} | 완료한 할일 ${checkedBox.length}`;
+    }
+}
+
+
+function todoDelete(e){
+    const button = e.target;
+    const li = button.parentNode;
+    li.remove();
+    // 제거 한 후의 상태 값을 가져와야 하기에 remove() 다음에 변수를 호출해야한다.
+    //const checkboxNumber = document.querySelectorAll("#todolist>li>input[type='checkbox']");
+    //const checkedBox = document.querySelectorAll("#todolist>li>input[type='checkbox']:checked");
+    //done.innerHTML = `전체 할일 ${checkboxNumber.length} | 완료한 할일 ${checkedBox.length}`;
+    count();
+}
+
+/* 강사님 코드 */
+/*
+function getAllcount(){
+    const todolist = document.querySelector("#todolist");
+    // 1) return을 통해서 값을 전달
+    // 2) DOM을 가져와서 출력
+    // 둘중에 편한 방법을 사용
+    console.log(todolist.childElementCount);
+}
+
+
+// 체크된 DON 갯수 가져오기
+function getCheckedCount(){
+    const checkedlist = document.querySelectorAll("#todolist li input[type='checkbox']:checked");
+}
+*/
